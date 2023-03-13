cd621ea160bc6543eac2480c433d9fbbdd14b172
Min Kyeong <kmg4love@naver.com>
2023-03-13 23:20:39 +0900

    first commit

--- hello.py
+++ hello.py
@@ -1,12 +1,11 @@
 class FourCal:
-    def __init__(self, first, second):
-        self.first=first
-        self.second=second
     def setdata(self, first, second):
         self.first=first
         self.second=second
     def add(self):
         result=self.first+self.second
         return result
-    
-a=FourCal(1,2)
+
+a=FourCal()
+a.setdata(4,2)
+print(a.add())

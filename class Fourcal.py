d958fe71d4b45db81146cf2f74e48db37e0743ce
Min Kyeong <kmg4love@naver.com>
2023-03-15 10:51:16 +0900

    first commit

---
+++ class Fourcal.py
@@ -0,0 +1,11 @@
+class FourCal:
+    def __init__(self, first, second):
+        self.first=first
+        self.second=second
+    def setdata(self, fisrt, second):
+        self.first=fisrt
+        self.second=second
+    def add(self):
+        result=self.first+self.second
+        return result
+a=Fourcal

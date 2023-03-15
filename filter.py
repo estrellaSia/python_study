f3b1a40dfa32c51d75d39f647aec72b018c1837d
Min Kyeong <kmg4love@naver.com>
2023-03-15 23:33:18 +0900

    first commit

---
+++ filter.py
@@ -0,0 +1,4 @@
+def positive(x):
+    return x>0
+a=list(filter(positive, [1,-3,2,0,-5,6]))
+print(a)
---
+++ 배수.py
@@ -0,0 +1,5 @@
+result=0
+for n in range(1,10):
+    if n %3==0 or n %5==0:
+        result+=n
+print(result)

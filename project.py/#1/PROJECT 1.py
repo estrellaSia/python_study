4c2bf9b0485d11fda6a1ced35696a7965b8d1892
Min Kyeong <kmg4love@naver.com>
2023-03-29 23:48:54 +0900

    first commit

---
+++ project.py/#1/PROJECT 1.py
@@ -0,0 +1,7 @@
+# PROJECT 1
+#밴드명 생성기
+
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name coule be " + city + " " + pet)
---
+++ project.py/#1/variables.py
@@ -0,0 +1,10 @@
+# 변수 a와 b에 저장된 값을 바꾸는 프로그램
+a = input("a: ")
+b = input("b: ")
+
+c=a
+a=b
+b=c
+
+print("a: " +a)
+print("b: " +b)

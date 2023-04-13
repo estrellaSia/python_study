b319c26d6a6097e236ecb302fb6b8d34a9d9be75
Min Kyeong <kmg4love@naver.com>
2023-04-13 21:00:02 +0900

    second commit

--- for.py
+++ for.py
@@ -78,3 +78,33 @@ for i in range(2, 10, 1) :
     for k in range(1, 10, 1) :
         print("%d X %d = %2d" %(i, k, i*k))
     print("")
+
+
+#구구단 표형태로 출력하는 프로그램
+i, k, guguLine = 0, 0, ""
+
+for i in range(2, 10) :
+    guguLine = guguLine + (" #  %d단  #" %i)
+
+print(guguLine)
+
+for i in range(1, 10) :
+    guguLine = ""
+    for k in range(2,10) :
+        guguLine = guguLine + str("%2dX %2d= %2d" % (k, i, k*i))
+    print(guguLine)
+    
+
+#구구단 표형태로 거꾸로 출력하는 프로그램
+i, k, guguLine = 0, 0, ""
+
+for i in range(9, 1, -1) :
+    guguLine = guguLine + (" #  %d단  #" %i)
+
+print(guguLine)
+
+for i in range(9, 0, -1) :
+    guguLine = ""
+    for k in range(9, 1, -1) :
+        guguLine = guguLine + str("%2dX %2d= %2d" % (k, i, k*i))
+    print(guguLine)

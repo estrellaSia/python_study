793a4e5273f0eb3907955dfe5811f05fc71bbfb4
Min Kyeong <kmg4love@naver.com>
2023-05-17 12:38:04 +0900

    first commit

---
+++ project.py/#3/odd, even number.py
@@ -0,0 +1,8 @@
+# if, else
+# 입력한 수가 짝수인지 홀수인지 구별하는 프록램
+
+number = int(input("Which number do you want to check? "))
+if number % 2 == 0:
+  print("This is an even number")
+else:
+  print("This is an odd number.")

9342ca1a098c5454e027694a60380b6e6d1ca41c
Min Kyeong <kmg4love@naver.com>
2023-05-24 14:12:31 +0900

    first commit

---
+++ project.py/#3/사랑 계산기.py
@@ -0,0 +1,34 @@
+# 두 사람의 이름을 입력받고,
+# 그 이름들 속의 t,r,u,e 갯수를 더한 값이 십의 자리에,
+# l,o,v,e 갯수를 더한 값이 일의 자리에 오게하여
+# 두 사람의 사랑을 계산하는 프로그램
+
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+combined_names = name1 + name2
+lower_names = combined_names.lower()
+t = lower_names.count("t")
+r = lower_names.count("r")
+u = lower_names.count("u")
+e = lower_names.count("e")
+first_digit = t + r + u + e
+
+l = lower_names.count("l")
+o = lower_names.count("o")
+v = lower_names.count("v")
+e = lower_names.count("e")
+second_digit = l + o + v + e
+
+score = int(str(first_digit) + str(second_digit))
+
+if (score < 10) or (score > 90):
+  print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+  print(f"Your score is {score}, you are alright together.")
+else:
+  print(f"Your score is {score}.")

f309579f04d04e8e16d65db6517d08a87148eb0f
Min Kyeong <kmg4love@naver.com>
2023-05-24 14:24:54 +0900

    주석 수정

--- project.py/#3/사랑 계산기.py
+++ project.py/#3/사랑 계산기.py
@@ -6,9 +6,6 @@
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
 
 combined_names = name1 + name2
 lower_names = combined_names.lower()
@@ -32,3 +29,4 @@ elif (score >= 40) and (score <= 50):
   print(f"Your score is {score}, you are alright together.")
 else:
   print(f"Your score is {score}.")
+  

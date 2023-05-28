fcff76a47eca3ebaf38ec2fe68d7e00f1721b582
Min Kyeong <kmg4love@naver.com>
2023-05-29 00:03:00 +0900

    second commit

--- 윈도 프로그래밍/9개 버튼을 2차원으로 배치하는 프로그램.py
+++ 윈도 프로그래밍/9개 버튼을 2차원으로 배치하는 프로그램.py
@@ -1,8 +1,8 @@
 from tkinter import*
 
-btnList = [None]*9
-fnameList = ["froyo.gif", "gingerbread.gif", "honeycomb.gif", "icecream.gif", "jelly bean.gif","kitkat.gif","lollipop.gif","marshmallow.gif","nougat.gif"]
-photoList = [None]*9
+btnList = [None] * 9
+fnameList = ["froyo.gif", "gingerbread.gif", "honeycomb.gif", "icecream.gif", "jellybean.gif", "kitkat.gif", "lollipop.gif", "marshmallow.gif", "nougat.gif"]
+photoList = [None] * 9
 i, k = 0, 0
 xPos, yPos = 0, 0
 num = 0
@@ -19,8 +19,6 @@ for i in range(0, 3):
         btnList[num].place(x = xPos, y = yPos)
         num += 1
         xPos += 70
-    xPos = 0
-    yPos += 70
     xPos = 0
     yPos += 70
 

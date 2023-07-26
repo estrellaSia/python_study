e452251f8512c42ab7b071612260ba078cfb499b
Min Kyeong <kmg4love@naver.com>
2023-07-26 14:01:49 +0900

    first commit

---
+++ Project/Intermediate/#18/Importing Modules, Installing Packages, and Working with Aliases.py
@@ -0,0 +1,8 @@
+import turtle
+tim = turtle.Turtle()
+
+from turtle import Turtle
+
+tim = Turtle()
+tom = Turtle()
+terry = Turtle()
---
+++ Project/Intermediate/#18/Turtle Challenge 1.py
@@ -0,0 +1,9 @@
+# Turtle Challenge 1 - Draw a Square
+
+from turtle import Turtle
+
+tim = Turtle()
+
+for _ in range(4):
+    tim.forward(100)
+    tim.left(90)

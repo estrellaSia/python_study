4468b9727cd4e2a81041db4505aadbfd3e59b54d
Min Kyeong <kmg4love@naver.com>
2023-08-16 21:12:14 +0900

    first commit:

---
+++ Project/Intermediate/#21 (Build the Snake Game Part 2).py/Step 6/main.py
@@ -0,0 +1,40 @@
+# Detect Collisions with the Wall
+
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black") #background color
+screen.title("My Snake Game")
+screen.tracer(0) #터틀 애니메이션 on/off, update를 하지 않는 이상 화면은 갱신되지 x기 때문에 어떤 동작을 수행하는지 볼 수 x
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up") #위쪽 방향키가 눌리면 Up 함수가 불릴 것임
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()  # 화면 갱신
+    time.sleep(0.1)  # 각 세그먼트가 이동하고 나서 0.1s 지연시간이 걸림(갱신 지연시키는 타이머를 써서 화면 갱신 횟수 조정)
+    snake.move()
+
+    #Detect collision with food
+    if snake.head.distance(food) < 15: #뱀의 머리부터 먹이까지의 거리
+        food.refresh()
+        scoreboard.increase_score()
+    #Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or  snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+screen.exitonclick()

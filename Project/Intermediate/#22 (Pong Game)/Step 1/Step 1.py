696b3bda12102edcc330c47bb93e940a7969ed5f
Min Kyeong <kmg4love@naver.com>
2023-08-21 23:12:25 +0900

    first commit

---
+++ Project/Intermediate/#22 (Pong Game)/Step 1/Step 1.py
@@ -0,0 +1,11 @@
+# Set up the Main Screen
+
+from turtle import Screen
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+
+
+screen.exitonclick()

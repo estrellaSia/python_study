79f9cc0ea4126ce60adcb7fe642905671403c3e9
Min Kyeong <kmg4love@naver.com>
2023-08-22 13:31:51 +0900

    first commit

---
+++ SW/리스트, 튜플, 딕셔너리/세트2.py
@@ -0,0 +1,7 @@
+mySet1 = {1, 2, 3, 4, 5}
+mySet2 = {4, 5, 6, 7}
+
+print(mySet1.intersection(mySet2)) # 교집합
+print(mySet1.union(mySet2)) # 합집합
+print(mySet1.difference(mySet2)) # 차집합
+print(mySet1.symmetric_difference(mySet2)) # 대칭 차집합

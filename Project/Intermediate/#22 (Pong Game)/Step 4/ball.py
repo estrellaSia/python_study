bf9db482a7fcd9302e5ec8cd94acffb9627126ca
Min Kyeong <kmg4love@naver.com>
2023-09-04 09:36:51 +0900

    modify

--- Project/Intermediate/#22 (Pong Game)/Step 4/ball.py
+++ Project/Intermediate/#22 (Pong Game)/Step 4/ball.py
@@ -6,9 +6,7 @@ class Ball(Turtle):
         super().__init__()
         self.shape("circle")
         self.color("white")
-        self.shapesize(stretch_wid=1, stretch_len=1)
         self.penup()
-        self.goto(0, 0)
 
     def move(self):
         new_x = self.xcor() + 10

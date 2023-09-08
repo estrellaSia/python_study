9ca8d75a45fe3e551cf43e6bc4eb60592276b261
Min Kyeong <kmg4love@naver.com>
2023-09-08 17:50:50 +0900

    first commit

---
+++ Project/Intermediate/#23 (Turtle Crossing Game)/Step 3/main.py
@@ -0,0 +1,33 @@
+# Detect collision with car
+
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+2
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+
+screen.exitonclick()

fb2407c7d2adc1d708af45167bbd5c623a36cf1b
Min Kyeong <kmg4love@naver.com>
2023-10-03 23:19:48 +0900

    first commit

---
+++ SW/리스트, 튜플, 딕셔너리/리스트 조작 함수.py
@@ -0,0 +1,7 @@
+# 리스트.sort() : 리스트 자체를 정렬함
+# 리스트.sorted() : 리스트는 그대로 두고 정렬된 새로운 리스트를 생성해 반환함
+
+myList = [30, 10, 20]
+newList = sorted(myList)
+print("sorted() 후의 myList : %s" %myList)
+print("sorted() 후의 mnewList : %s" %newList)

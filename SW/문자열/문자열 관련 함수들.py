02e6d1edb9b2b803a7256d67fcc1154d11487ea9
Min Kyeong <kmg4love@naver.com>
2023-11-23 23:38:43 +0900

    commit

--- SW/문자열/문자열 관련 함수들.py
+++ SW/문자열/문자열 관련 함수들.py
@@ -1,9 +1,13 @@
-# count() - 문자 개수 세기
+# 문자 개수 세기 - count()
 a = "hobby"
 print(a.count('b'))
 
-# find() - 위치 알려 주기
+# 위치 알려 주기 - find()
 s = "hello python!"
 print(s.find('p'))
 
 print(s.find('k')) # 해당 문자가 없다면 -1을 반환
+
+# 위치 알려 주기 - index()
+b = "Life is too short"
+print(b.index('t'))

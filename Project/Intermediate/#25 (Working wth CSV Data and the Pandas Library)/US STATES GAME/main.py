93a831ab9597373a00bfff2a249b1f4b9e3f0da8
Min Kyeong <kmg4love@naver.com>
2023-11-27 22:45:03 +0900

    commit

--- Project/Intermediate/#25 (Working wth CSV Data and the Pandas Library)/US STATES GAME/main.py
+++ Project/Intermediate/#25 (Working wth CSV Data and the Pandas Library)/US STATES GAME/main.py
@@ -2,12 +2,9 @@ import turtle
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
-image = "blank_state_img.gif" #실제 파일 이름과 똑같이 써야 함, 그렇지 않으면 _tkinter.TclError: couldn't open "blank_state_img.gif": no such file or directory 오류 날 수 있음
+image = "blank_states_img.gif" #실제 파일 이름과 똑같이 써야 함, 그렇지 않으면 _tkinter.TclError: couldn't open "blank_state_img.gif": no such file or directory 오류 날 수 있음
 screen.addshape(image)
 turtle.shape(image)
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
-
-turtle.onscreenclick(get_mouse_click_coor)
-turtle.mainloop()
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?") # 문자열 입력을 위한 대화 상자 창을 띄움, 매개변수 title=> 대화 상자 창의 제목, prompt=> 주로 어떤 정보를 입력해야 하는지 설명하는 텍스트
+print(answer_state)

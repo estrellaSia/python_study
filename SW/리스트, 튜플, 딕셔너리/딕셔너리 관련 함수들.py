51de9a3d4060fb12d64c9d246101f84eadae70f4
Min Kyeong <kmg4love@naver.com>
2023-12-11 14:24:26 +0900

    commit

--- SW/리스트, 튜플, 딕셔너리/딕셔너리 관련 함수들.py
+++ SW/리스트, 튜플, 딕셔너리/딕셔너리 관련 함수들.py
@@ -1,3 +1,6 @@
 # keys() - key 리스트 만들기
 a = {'name':'mk', 'phone' : '010-1234-5678', 'birth' : '1112'}
-print(a.keys())
+print(a.keys()) # 결과값: dict_keys(['name', 'phone', 'birth'])
+
+# values() - value 리스크 만들기
+print(a.values()) # 결과값: dict_values(['mk', '010-1234-5678', '1112'])
